33380f266862b373eabbcec238662a2e9f5db9f2
Rodrigow897 <rwesley2000@gmail.com>
2025-06-16 21:55:10 -0300

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,24 @@ function challenge01() {
 }
 // 02-Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
 function challenge02() {
-   var n1 = parseInt(window.prompt("Digite um numero: "))
-   var n2 = parseInt(window.prompt("Digite o segundo numero: "))
-
-   window.alert(`o resultado de ${n1} + ${n2} é ${(n1 + n2)}`)
+    var n1 = parseInt(window.prompt("Digite o primeiro número: "));
+    if (isNaN(n1) || n1 === null) {
+        window.alert("Voc^não digitou um número!")
+        return;
+    }
+    var n2 = parseInt(window.prompt("Digite o segundo número: "))
+    if (isNaN(n2) || n2 === null) {
+        window.alert("Você não digitou um número!")
+        return;
+    }
+    window.alert(`O resultado de ${n1} + ${n2} é ${n1 + n2}`);
 }
 
 // 03 - Crie um script que declara uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
 function challenge03() {
     var num = window.prompt("Digite um numero: ")
-    if (isNaN(num)) {
-        window.alert("Não é um numero!")
+    if (isNaN(num) || num === null){
+        window.alert("Não é um número!")
     } else {
         window.alert("é um numero!")
     }
@@ -22,29 +29,98 @@ function challenge03() {
 
 // 04 - Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
 function challenge04() {
-    var nome = window.prompt("Digite um nome: ")
-    if (isNaN(nome)) {
-        window.alert("é uma string!")
+    var nome = 455
+    if(typeof nome === 'string') {
+        window.alert("É uma string")
     } else {
-        window.alert("não é uma string!")
+        window.alert("Não é uma string")
     }
 }
 
-
 // 05 - Crie um script que declara uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
-
+function challenge05() {
+    var verf = true
+    if (typeof verf === 'boolean') {
+        window.alert("É booleano")
+    } else {
+        window.alert("Não é booleano")
+    }
+}
 
 // 06 - Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
-
+function challenge06() {
+   var n1 = parseInt(window.prompt("Digite um numero: "))
+   if(isNaN(n1) || n1 === null){
+    window.alert("Você não digitou um número: ")
+    return;
+   }
+   var n2 = parseInt(window.prompt("Digite o segundo numero: "))
+    if(isNaN(n2) || n2 === null){
+    window.alert("Você não digitou um número: ")
+    return;
+    }
+   window.alert(`o resultado de ${n1} - ${n2} é ${(n1 - n2)}`)
+}
 
 // 07 - Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+function challenge07() {
+   var n1 = parseInt(window.prompt("Digite um numero: "))
+   if(isNaN(n1) || n1 === null) {
+    window.alert("Você nào digitou um número!")
+    return;
+   }
+   var n2 = parseInt(window.prompt("Digite o segundo numero: "))
+   if(isNaN(n2) || n2 === null) {
+    window.alert("Você nào digitou um número!")
+    return;
+   }
 
+   window.alert(`o resultado de ${n1} * ${n2} é ${(n1 * n2)}`)
+}
 
 // 08 - Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+function challenge08() {
+   var n1 = parseInt(window.prompt("Digite um numero: "))
+   if(isNaN(n1) || n1 === null) {
+    window.alert("Você nào digitou um número!")
+    return;
+   }
+   var n2 = parseInt(window.prompt("Digite o segundo numero: "))
+   if(isNaN(n2) || n2 === null) {
+    window.alert("Você nào digitou um número!")
+    return;
+   }
 
+   if(n2 === 0) {
+    window.alert("Não é possivel dividir por 0")
+    return;
+   }
+
+   window.alert(`o resultado de ${n1} ÷ ${n2} é ${(n1 / n2)}`)
+}
 
 // 09 - Crie um script que declara uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+function challenge09() {
+    var num = parseInt(window.prompt("Digite um numero: "))
 
+    if(isNaN(num) || num === null) {
+        window.alert("você não digitou um numero: ")
+    } else if (num % 2 === 0) {
+        window.alert("É um número par")
+    } else {
+        window.alert("Não é um número par")
+    }
+}
 
 // 10-  Crie um script que declara uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+function challenge10() {
+    var num = parseInt(window.prompt("Digite um numero: "))
 
+    if(isNaN(num) || num === null) {
+        window.alert("você não digitou um numero: ")
+    } else if (num % 2 != 0) {
+        window.alert("É um número impar")
+    } else {
+        window.alert("Não é um número impar")
+    }
+}
